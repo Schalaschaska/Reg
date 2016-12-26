58a8e6856e080d65bc2f829d8d4ccbb7162ef0ce
Schalaschaska <mihanhlystow@gmail.com>
2016-12-26 16:27:57 +0300

    com2

--- Reg/MainWindow.xaml.cs
+++ Reg/MainWindow.xaml.cs
@@ -78,7 +78,7 @@ namespace Reg
                 }
                 if(comboBox.Text=="Комплексное число")
                 {
-                    Regex Reg_k = new Regex(@"^\s*[+-]?((\d+)?[.,])?(\d*)?\s*[+-]\s*((\d+)?[.,])?(\d+)?[*_x]?[i]\s*$");
+                    Regex Reg_k = new Regex(@"^\\s*[+-]?((\d+)?[.,])?(\d*)?\s*[+-]\s*((\d+)?[.,])?(\d+)?[*_x]?[i]\s*$");
                     if(Reg_k.IsMatch(textBox.Text))
                     {
                         MessageBox.Show("Это комплексное число");
@@ -102,7 +102,7 @@ namespace Reg
                 }
                 if(comboBox.Text=="html контейнер")
                 {
-                    Regex Reg_html = new Regex(@"^<\w*>\w*\</\w*\>$");//сравнить по строкам
+                    Regex Reg_html = new Regex(@"^<\w*>\w*\</\w*\>$");
                     if(Reg_html.IsMatch(textBox.Text))
                     {
                         MessageBox.Show("Это html контейнер");
@@ -124,9 +124,21 @@ namespace Reg
                         MessageBox.Show("Это не URL");
                     }
                 }
+                if (comboBox.Text == "Адрес")
+                {
+                    Regex Reg_ad = new Regex(@"^([0-9]{6})\s([а-яА-Яa-zA-Z ]+)\s(ул\.)([а-яА-Яa-zA-Z0-9 ]+)\s(д\.)([0-9а-яА-Яa-zA-Z\-]+)\s(кв\.)([0-9]+)$");
+                    if (Reg_ad.IsMatch(textBox.Text))
+                    {
+                        MessageBox.Show("Это адрес");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Это не адрес");
+                    }
+                }
                 if (comboBox.Text == "ФИО")
                 {
-                    Regex Reg_Fio = new Regex(@"^[А-ЯA-Z][а-яa-zА-ЯA-Z\-]{0,}\s[А-ЯA-Z][а-яa-zА-ЯA-Z\-]{1,}(\s[А-ЯA-Z][а-яa-zА-ЯA-Z\-]{1,})?$");
+                    Regex Reg_Fio = new Regex(@"^[(А-ЯA-Z)|[(а-я)][а-яa-zА-ЯA-Z\-]{0,}\s[А-ЯA-Z][а-яa-zА-ЯA-Z\-]{1,}(\s[А-ЯA-Z][а-яa-zА-ЯA-Z\-]{1,})?$");
                     if (Reg_Fio.IsMatch(textBox.Text))
                     {
                         MessageBox.Show("Это ФИО");
@@ -138,7 +150,7 @@ namespace Reg
                 }
                 if (comboBox.Text == "Телефонный номер")
                 {
-                    Regex Reg_tel = new Regex(@"^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$");
+                    Regex Reg_tel = new Regex(@"^(8\d{10}|\+7\d{10}|8\(\d{3}\)\d{7}|\+7\(\d{3}\)\d{7}|8\ \(\d{3}\)\ \d{7}|\+7\ \(\d{3}\)\ \d{7}|8\ \(\d{3}\)\ (\d{2}\-){2}\d{3}|8\ \(\d{3}\)\ (\d{2}\ \-\ ){2}\d{3})$");
                     if (Reg_tel.IsMatch(textBox.Text))
                     {
                         MessageBox.Show("Это телефонный номер");
@@ -182,7 +194,15 @@ namespace Reg
                 }
                 if(comboBox.Text=="Правописание")
                 {
-                    textBox.SpellCheck.IsEnabled = true;
+                    Regex Reg_prav = new Regex(@"^(((.*?[Ж|Ш|ж|ш]ы)+)|(.*?([Ж|Ш|ж|ш]ы).*)|((.*?[Ч|Щ|ч|щ]а)+)|(.*?([Ч|Щ|ч|щ][а|ю]).*))$");
+                    if (Reg_prav.IsMatch(textBox.Text))
+                    {
+                        MessageBox.Show("Есть ошибки");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Нет ошибок");
+                    }
                 }
                 if(comboBox.Text=="Проверка даты")
                 {
@@ -197,6 +217,33 @@ namespace Reg
                     {
                         MessageBox.Show("Дата введена некорректно");
                     }
+                }
+                if (comboBox.Text == "Проверка даты(2)")
+                {
+                    Regex dat = new Regex(@"^(((0[1-9]|[1|2][0-9]|3[0|1])[\-|\ |\/|\.](0[1|3|5|7|8]|1[0|2])[\-|\ |\/|\.]((19|20)\d{2}))|((0[1-9]|[1|2][0-9]|30)[\-|\ |\/|\.](0[4|6|9]|11)[\-|\ |\/|\.](19|20)\d{2})|((0[1-9]|[1|2][0-9])[\-|\ |\/|\.](02)[\-|\ |\/|\.](19|20)\d{2}))$");
+                    if (dat.IsMatch(textBox.Text))
+                    {
+                        MessageBox.Show("Дата корректа");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Дата некорректна");
+                    }
+                   
+                       
+                }
+                if(comboBox.Text== "Целое число с разделение разрядов")
+                {
+                    Regex zr = new Regex(@"^(((\d+\ )+|(\d+)))+$");
+                    if(zr.IsMatch(textBox.Text))
+                    {
+                        MessageBox.Show("Корректный ввод");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Некорректный ввод");
+                    }
+
                 }
 
 
